1d384b296c8ab240acc16a2a94ffac1d6a9729fd
Yong Yang <yongyang3800@gmail.com>
2016-09-07 18:59:57 -0500

    Maven

---
+++ src/main/java/edu/iit/cs445/f2016/homework1/App.java
@@ -0,0 +1,13 @@
+package edu.iit.cs445.f2016.homework1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
